ff9f713bd6facc1822d6b6e1748104c815d62eb9
Lumirelle <shabbyacc@outlook.com>
2026-01-01 21:10:51 +0800

    chore: add knip

---
+++ knip.config.ts
@@ -0,0 +1,10 @@
+import type { KnipConfig } from 'knip'
+
+export default {
+  // @keep-sorted
+  ignoreDependencies: [
+    '@antfu/utils',
+    'bumpp',
+    'lint-staged',
+  ],
+} satisfies KnipConfig
