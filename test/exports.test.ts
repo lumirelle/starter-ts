228b1cea1f7087294c17aff1f6e2a34e86ae5178
Lumirelle <shabbyacc@outlook.com>
2026-01-08 16:10:36 +0800

    test: support glob

--- test/exports.test.ts
+++ test/exports.test.ts
@@ -1,6 +1,6 @@
-import { YAML } from 'bun'
+import { Glob, YAML } from 'bun'
 import { describe, expect, it } from 'bun:test'
-import { join } from 'node:path'
+import { dirname, join } from 'node:path'
 import { getPackageExportsManifest } from 'vitest-package-exports'
 import { workspaces } from '../package.json'
 
@@ -8,23 +8,26 @@ import { workspaces } from '../package.json'
 const IS_READY = false
 
 describe.todoIf(!IS_READY)('exports-snapshot', async () => {
+  const root = join(import.meta.dir, '..')
   /**
    * FIXME: If `bun` support command like `pnpm ls --only-projects`, we may no longer need this, see https://github.com/oven-sh/bun/issues/25114
    */
-  const pkgPaths = workspaces.packages
-  for (const pkgPath of pkgPaths) {
-    const pkgJson = await import(join(import.meta.dir, '..', pkgPath, 'package.json')).then(m => m.default)
-    if (pkgJson.private)
-      continue
-    it(`${pkgJson.name}`, async () => {
-      const manifest = await getPackageExportsManifest({
-        importMode: 'src',
-        cwd: pkgPath,
+  for (const pkg of workspaces.packages) {
+    const glob = new Glob(`${pkg}/package.json`)
+    for await (const pkgJsonPath of glob.scan({ cwd: root, absolute: true })) {
+      const pkgJson = await import(pkgJsonPath).then(m => m.default)
+      if (!pkgJson.name || pkgJson.private)
+        continue
+      it(`${pkgJson.name}`, async () => {
+        const manifest = await getPackageExportsManifest({
+          importMode: 'src',
+          cwd: dirname(pkgJsonPath),
+        })
+        expect(YAML.stringify(manifest.exports))
+        // .toMatchFileSnapshot(`./exports/${pkg.name}.yaml`)
+        // TODO: Workaround. Bun currently does not support file snapshot like Vitest, see https://github.com/oven-sh/bun/issues/13096
+          .toMatchInlineSnapshot()
       })
-      expect(YAML.stringify(manifest.exports))
-      // .toMatchFileSnapshot(`./exports/${pkg.name}.yaml`)
-      // TODO: Workaround. Bun currently does not support file snapshot like Vitest, see https://github.com/oven-sh/bun/issues/13096
-        .toMatchInlineSnapshot()
-    })
+    }
   }
 })
