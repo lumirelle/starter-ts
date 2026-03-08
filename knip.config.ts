8bd48af6a6066c4fe1ec71ecf951240970c4e782
Lumirelle <shabbyacc@outlook.com>
2026-03-08 16:22:33 +0800

    chore: deprecate oxfmt

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,7 @@
 /* eslint perfectionist/sort-objects: "error" */
 import type { KnipConfig } from 'knip'
 import { name } from './package.json'
+
 export default {
   ignoreDependencies: [name, '@antfu/utils', '@arethetypeswrong/cli', 'publint'],
 } satisfies KnipConfig
