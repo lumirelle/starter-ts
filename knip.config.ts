84b121ff52c330f4c75809b96256c76e460f59cd
Lumirelle <shabbyacc@outlook.com>
2026-05-09 09:18:46 +0800

    chore: re-organize deps & update tsconfig & update knip config

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,5 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  /// keep-sorted
   ignoreDependencies: ['@arethetypeswrong/cli', '@lumirelle/oxlint-config', 'publint'],
 } satisfies KnipConfig
