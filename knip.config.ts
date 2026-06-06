5043a21c97d77e944e5408c567ba003c5f3d753c
Lumirelle <shabbyacc@outlook.com>
2026-06-07 04:21:07 +0800

    build: migrate from bun pm to aube

--- knip.config.ts
+++ knip.config.ts
@@ -3,5 +3,5 @@ import type { KnipConfig } from 'knip'
 export default {
   entry: ['test/**/*.{ts,js}'],
   ignoreBinaries: ['mise'],
-  ignoreDependencies: ['@lumirelle/oxlint-config', 'bumpp', 'nano-staged'],
+  ignoreDependencies: ['@lumirelle/oxlint-config', 'nano-staged'],
 } satisfies KnipConfig
